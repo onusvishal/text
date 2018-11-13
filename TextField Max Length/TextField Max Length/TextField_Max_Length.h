702ea8a29206b6d93c83f14f80f5afceca8e5d62
IOS <>
2018-11-13 16:59:00 +0530

    no message

---
+++ TextField Max Length/TextField Max Length/TextField_Max_Length.h
@@ -0,0 +1,19 @@
+//
+//  TextField_Max_Length.h
+//  TextField Max Length
+//
+//  Created by Admin on 13/11/18.
+//  Copyright © 2018 VISHAL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TextField_Max_Length.
+FOUNDATION_EXPORT double TextField_Max_LengthVersionNumber;
+
+//! Project version string for TextField_Max_Length.
+FOUNDATION_EXPORT const unsigned char TextField_Max_LengthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TextField_Max_Length/PublicHeader.h>
+
+
